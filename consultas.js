ffe4b1baf51b42ac4e2b7941ea30baf75960e878
Sebastian Martinez <sebamartinez1797@gmail.com>
2024-12-09 13:36:43 -0300

    cambios

--- consultas.js
+++ consultas.js
@@ -1,34 +1,33 @@
-
-const {Pool} = require('pg')
+const { Pool } = require("pg");
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'likeme',
-    allowExitOnIdle:true,
-})
+  host: "localhost",
+  user: "postgres",
+  password: "79293224",
+  database: "likeme",
+  allowExitOnIdle: true,
+});
 
-const agregarPost = async(titulo, url, descripcion) =>{
-    const consulta = "INSERT INTO posts values (DEFAULT, $1, $2, $3) RETURNING id" // Add RETURNING id
-    const values = [titulo, url, descripcion]
-    try {
-        const result = await pool.query(consulta, values)
-        if (result.rowCount > 0) {
-            const postId = result.rows[0].id;
-            console.log("Post agregado con ID:", postId);
-        } else {
-            console.warn("Post no agregado. No se insertaron filas.");
-        }
-    } catch (error) {
-        console.error("Error al agregar post:", error);
+const agregarPost = async (titulo, url, descripcion) => {
+  const consulta =
+    "INSERT INTO posts values (DEFAULT, $1, $2, $3) RETURNING id"; // Add RETURNING id
+  const values = [titulo, url, descripcion];
+  try {
+    const result = await pool.query(consulta, values);
+    if (result.rowCount > 0) {
+      const postId = result.rows[0].id;
+      console.log("Post agregado con ID:", postId);
+    } else {
+      console.warn("Post no agregado. No se insertaron filas.");
     }
-}
+  } catch (error) {
+    console.error("Error al agregar post:", error);
+  }
+};
 
-const getPosts = async() =>{
-    const {rows} = await pool.query("SELECT * FROM posts")
-    console.log(rows)
-    return rows
-}
+const getPosts = async () => {
+  const { rows } = await pool.query("SELECT * FROM posts");
+  console.log(rows);
+  return rows;
+};
 
-module.exports = {agregarPost, getPosts}
-
+module.exports = { agregarPost, getPosts };
--- index.js
+++ index.js
@@ -1,29 +1,22 @@
+const express = require("express");
+const { agregarPost, getPosts } = require("./consultas");
+const app = express();
 
-const express = require('express')
-const { agregarPost, getPosts } = require('./consultas')
-const app = express()
-
-
-const cors = require('cors')
+const cors = require("cors");
 
 app.use(express.json());
 
-app.listen(3000, console.log('Servidor encendido'))
+app.listen(3000, console.log("Servidor encendido"));
 
-app.use(cors())
+app.use(cors());
 
+app.get("/posts", async (req, res) => {
+  const posts = await getPosts();
+  res.json(posts);
+});
 
-
-
-app.get("/posts", async (req,res) =>{
-    const posts = await getPosts()
-    res.json(posts)
-})
-
-
-
-app.post("/posts", async (req, res) =>{
-    const { titulo, url, descripcion } = req.body
-    await agregarPost(titulo, url, descripcion)
-    res.send("Post agregado con éxito")
-})
+app.post("/posts", async (req, res) => {
+  const { titulo, url, descripcion } = req.body;
+  await agregarPost(titulo, url, descripcion);
+  res.send("Post agregado con éxito");
+});
